544cd7bacdb5f7ebeed29359bbe24bdfbb5382f2
tuplenchwar <tuplenchwar@gmail.com>
2025-03-08 22:31:07 -0800

    working publisher

--- Publisher/src/main/java/org/example/controller/PublisherController.java
+++ Publisher/src/main/java/org/example/controller/PublisherController.java
@@ -1,19 +1,19 @@
 package org.example.controller;
 
-import org.example.service.PublisherService;
+import org.example.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import java.util.List;
 
-@RestController  // Keep @RestController
+@RestController
 @RequestMapping("/publisher")
 public class PublisherController {
 
-    private final PublisherService publisherService;
 
-    public PublisherController(PublisherService publisherService) {
-        this.publisherService = publisherService;
-    }
+    @Autowired
+    private PublisherService publisherService;
+
 
     // API to Get Topics
     @GetMapping("/topics")
